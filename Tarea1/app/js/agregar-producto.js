ab0481bd305a12c2b929ade60d804164bd6f2c1f
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-04-07 13:39:58 -0400

    Avances validacion

--- Tarea1/app/js/agregar-producto.js
+++ Tarea1/app/js/agregar-producto.js
@@ -1,11 +1,20 @@
-const tipoProducto = document.getElementById("tipoProducto");
+//Llamamos a los elementos que requieren validación:
+const tipoProductos = document.querySelectorAll('.tipoProducto');
 const producto = document.getElementById("producto");
 const region = document.getElementById("region");
 const comuna = document.getElementById("comuna");
+const fotos = document.querySelectorAll('input[type="file"]');
+const nombre = document.getElementById("nombreProductor");
+const email = document.getElementById("emailProductor");
+const numero = document.getElementById("celularProductor");
+
+
 const agregarProductoBtn = document.getElementById("agregarProducto");
 const confirmationMessage = document.getElementById("confirmationMessage");
 const confirmarBtn = document.getElementById("confirmar");
 const cancelarBtn = document.getElementById("cancelar");
+
+
 const frutas = [
     "Arándano", "Frambuesa", "Frutilla", "Grosella", "Mora", "Limón", "Mandarina",
     "Naranja", "Pomelo", "Melón", "Sandía", "Palta", "Chirimoya", "Coco", "Dátil",
@@ -20,137 +29,43 @@ const verduras = [
     "Berenjena", "Papa", "Pimiento", "Tomate", "Zanahoria"
 ];
 
-// Función para llenar el menú de selección de productos según el tipo seleccionado
-tipoProducto.addEventListener("change", function() {
-    // Limpiar el menú de selección de productos antes de agregar nuevas opciones
-    while (producto.firstChild) {
-        producto.removeChild(producto.firstChild);
+const validarTipo = (tipoProductos) => {
+  let conteoTiposValidos = 0;
+  const tiposValidos = ["fruta", "verdura"];
+  
+  tipoProductos.forEach(function(select) {
+    const tipoSeleccionado = select.value;
+    if(tiposValidos.includes(tipoSeleccionado)){
+      conteoTiposValidos++;
+    }  
+  });
+  
+  return conteoTiposValidos > 0;
+};
+
+const validarFotos = (fotos) => {
+  let conteoFotos = 0;
+
+  fotos.forEach(function(input) {
+    if (input.files.length > 0) {
+      conteoFotos++;
     }
-    if (tipoProducto.value === "fruta") {
-        generarCheckboxes(frutas)
-    } else if (tipoProducto.value === "verdura") {
-       generarCheckboxes(verduras)
-    }
-});
+  });
+  return conteoFotos > 0;
+};
 
-const generarCheckboxes = (tipo) =>{
-    // Limpiar productosDiv
-    const productosDiv = document.getElementById("productos");
+const validarNombre = (nombre) => {
+  const largo = (nombre.value.trim()).length
+  return (largo>=3) && (largo <= 80);
+};
 
-    // Iterar sobre la lista de productos y crear un checkbox para cada uno
-    tipo.forEach(function(producto) {
-      // Crear el checkbox
-      const checkbox = document.createElement("input");
-      checkbox.type = "checkbox";
-      checkbox.name = "producto";
-      checkbox.value = producto;
+const validarEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
-      // Agregar un event listener para controlar la cantidad de elementos seleccionados
-      checkbox.addEventListener("change", function() {
-        const checkboxesSeleccionados = productosDiv.querySelectorAll("input[type='checkbox']:checked");
-        if (checkboxesSeleccionados.length > 5) {
-          this.checked = false; // Desmarcar el checkbox si se excede el límite de selección
-        }
-      });
-
-      // Crear la etiqueta para el checkbox
-      const label = document.createElement("label");
-      label.textContent = producto;
-
-      // Agregar el checkbox y la etiqueta al contenedor
-      productosDiv.appendChild(checkbox);
-      productosDiv.appendChild(label);
-      productosDiv.appendChild(document.createElement("br")); // Salto de línea
-    });
-  };
-
-  // Llamar a la función para generar los checkboxes
-
-
-// Función para llenar el menú de selección de comunas según la región seleccionada
-region.addEventListener("change", function() {
-    // Lógica para llenar el menú de selección de comunas según la región seleccionada
-});
-
-const validateEmail = email => {
-    // Email validation using a regular expression
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-  
-  const handleFormSubmit = () => {
-    console.log("Validating form...");
-  
-    const emailInput = document.getElementById("email");
-    const userNameInput = document.getElementById("username");
-    const passwordInput = document.getElementById("contrasenna");
-  
-    let isValid = true;
-    let errorMessage = "";
-  
-    // Validate inputs
-    if (!validateEmail(emailInput.value)) {
-      isValid = false;
-      errorMessage += "Por favor, ingresa un correo electrónico válido.\n";
-      emailInput.style.borderColor = "red";
-    } else {
-      emailInput.style.borderColor = "";
-    }
-  
-    if (!validateUserName(userNameInput.value)) {
-      isValid = false;
-      errorMessage += "Por favor, ingresa un nombre de usuario válido (5 caracteres mínimo).\n";
-      userNameInput.style.borderColor = "red";
-    } else {
-      userNameInput.style.borderColor = "";
-    }
-  
-    if (!validatePassword(passwordInput.value)) {
-      isValid = false;
-      errorMessage += "La contraseña debe tener al menos 8 caracteres, incluyendo un número, una minúscula y una mayúscula.\n";
-      passwordInput.style.borderColor = "red";
-    } else {
-      passwordInput.style.borderColor = "";
-    }
-  
-    // Handle errors or redirect to confessions page
-    if (!isValid) {
-      alert(errorMessage); // Replace with a user-friendly error display
-    } else {
-      // Store username in localStorage
-      const username = userNameInput.value;
-      localStorage.setItem("username", username);
-  
-      // Redirect to confessions page
-      window.location.href = "../html/confesiones.html";
-    }
-  };
-  
-  // --- Event Listener ---
-  
-  const submitButton = document.getElementById("confirmationMessage");
-  submitButton.addEventListener("click", handleFormSubmit);
-// Evento click para el botón "Agregar Producto(s)"
-agregarProductoBtn.addEventListener("click", function() {
-    if (validarFormulario()) {
-        // Si la validación pasa, mostrar mensaje de confirmación
-        confirmationMessage.style.display = "block";
-    } else {
-        // Si la validación falla, mostrar mensaje de error
-        alert("Por favor complete todos los campos obligatorios (*) correctamente.");
-    }
-});
-
-// Evento click para el botón "Sí, confirmo"
-confirmarBtn.addEventListener("click", function() {
-    // Mostrar mensaje de confirmación final
-    alert("Hemos recibido el registro de producto. Muchas gracias.");
-    // Redireccionar a la portada (index.html)
-    window.location.href = "index.html";
-});
-
-// Evento click para el botón "No, quiero volver al formulario"
-cancelarBtn.addEventListener("click", function() {
-    // Ocultar mensaje de confirmación y volver al formulario
-    confirmationMessage.style.display = "none";
-});
+const validarNumero = (numero) => {
+  const num = numero.value.trim()
+  const validez = /^\+569\d{8}$/.test(num);
+  return validez;
+}
