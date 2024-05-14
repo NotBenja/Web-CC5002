50f303bb25aa59856e873b09d213bcf3bfaf84eb
NotBenja <benjamin.reyes.b@ug.uchile.cl>
2024-05-14 04:13:47 -0400

    T2 terminada y validada
    Por fin XD

--- Tareas/Tarea2/app/static/js/ver-productos.js
+++
@@ -1,8 +0,0 @@
-let tabla = document.getElementById("tablaProductos");
-for (let i = 1; i < tabla.rows.length; i++) {
-  let fila = tabla.rows[i];
-  fila.addEventListener("click",function(){
-    window.location.href = "informacion-producto.html";
-  })   
-  }
-
